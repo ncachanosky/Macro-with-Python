c8d420b09f9cb2b10bf1544bac207167c0ca3702
Nicolas Cachanosky <44596340+ncachanosky@users.noreply.github.com>
2021-05-24 13:15:58 -0600

    AD-AS

--- static/Python/The AD-AS model.py
+++ static/Python/The AD-AS model.py
@@ -8,14 +8,22 @@
 #|============================================================================|
 #|============================================================================|
 
-#%%
+#%% *** CELL 1 ***
+"============================================================================"
 "1|IMPORT PACKAGES"
 import numpy             as np       # Package for scientific computing
-import matplotlib.pyplot as plt      # Matplotlib is a 2D plotting library
+import matplotlib.pyplot as plt      # Matplotlib is a plotting library
 from   scipy.optimize    import root # Package to find the roots of a function
 
-#%%
-"2|BUILD AD CLASS"
+
+"============================================================================"
+"2|PARAMETERS"
+# Model parameters
+dpi = 300
+
+
+"============================================================================"
+"3|BUILD AD CLASS"
 class class_AD:
     "Define the parameters of the model"
     def __init__(self, a     = 20   ,  # AD: autonomous consumption 
@@ -56,7 +64,7 @@ class class_AD:
         #Unpack the parameters (simplify notation)
         lmbda = self.lmbda
         rho   = self.rho
-        #Calculate m
+        #Calculate money multiplier (m)
         return ((1 + lmbda)/(rho + lmbda))
    
     "Money supply"     
@@ -82,41 +90,53 @@ class class_AD:
         c2    = self.c2
         c3    = self.c3
         #Calculate AD
-        AD_level1 = (((a-alpha)-(b-beta)*T+I+G+X)/d-c1/c3)
-        AD_level2 = ((1-b+beta)/d-c2/c3)**(-1)
+        AD_level1 = (((a-alpha) - (b-beta)*T + I + G + X)/d - c1/c3)
+        AD_level2 = ((1-b+beta)/d - c2/c3)**(-1)
         AD_shape  = self.M()/c3 * ((1-b+beta)/d - c2/c3)**(-1)
         return (AD_level1 * AD_level2 + AD_shape/P)
 
-"3|SHOW RESULTS"
+
+"============================================================================"
+"4|SHOW RESULTS"
 out = class_AD()
 
 print("Money multiplier =", round(out.m(), 2))
 print("Money supply ="    , round(out.M(), 2))
 
+
+"============================================================================"
+"5|CREATE MODEL DOMAIN AND POPULATE AD"
 size = 50
 P = np.linspace(1, size, size*2)
 Y = out.AD(P)
 
-"4|PLOT AD"
+
+"============================================================================"
+"6|PLOT AD"
+### AXIS RANGE
 y_max = np.max(P)
 x_max = np.max(Y)
-v = [0, x_max, 0, y_max]
-fig, ax = plt.subplots(figsize=(10, 8))
+axis_range = [0, x_max, 0, y_max]
+
+### BUILD PLOT
+fig, ax = plt.subplots(figsize=(10, 8), dpi=dpi)
 ax.set(title="AGREGGATE DEMAND", xlabel="Y", ylabel="P")
 ax.plot(Y, P, "k-")
+### AXIS
 ax.yaxis.set_major_locator(plt.NullLocator())  # Hide ticks
 ax.xaxis.set_major_locator(plt.NullLocator())  # Hide ticks
+### SETTINGS
+plt.axis(axis_range)
 plt.show()
 
-#%%
-"5|DEFINE PARAMETERS"
+
+#%% *** CELL 2 ***
+"============================================================================"
+"7|DEFINE PARAMETERS"
 # PRODUCTION FUNCTION
 A      =   1     # Total Factor Productivity
-varphi =   0.70  # Output elasticity of capital
-# CAPITAL STOCK
-s      =   0.25  # Savings rate
-delta  =   0.20  # Depreciation rate
 K      = 100     # Capital stock
+varphi =   0.70  # Output elasticity of capital
 # LABOR SUPPLY
 w_s    =   0.75  # Labor supply "slope" with respect to P
 # MONEY SUPPLY
@@ -134,7 +154,9 @@ c1     = 200     # Money demand: Precuationary
 c2     =   0.6   # Money demand: Transactions
 c3     =  10     # Money demand: Speculation
 
-"6|FUNCTIONS"
+
+"============================================================================"
+"8|FUNCTIONS"
 # LABOR SUPPLY
 def N(P):
     N = w_s * P
@@ -146,8 +168,8 @@ def output(P):
     return output
 
 # MONEY SUPPLY
-m = (1+lmbda)/(rho+lmbda)   # Money Multiplier
-M = B*m                     # Money Supply
+m = (1+lmbda)/(rho+lmbda)  # Money Multiplier
+M = B*m                    # Money Supply
 
 # AGGREGATE DEMAND
 def AD(P):
@@ -157,7 +179,9 @@ def AD(P):
     AD = AD_level1 * AD_level2 + AD_shape/P
     return AD
 
-"7|EQUILIBRIUM: PRICE LEVEL"
+
+"============================================================================"
+"9|EQUILIBRIUM: PRICE LEVEL"
 def equation(P):
     Eq1 = AD(P) - output(N(P))
     equation = Eq1
@@ -176,34 +200,34 @@ LRAS = output(N(Pstar))
 SRAS = output(N(P_vector))
 AD_vector = AD(P_vector)
 
-"8|PLOT AD-AS MODEL"
-v = [0, 80, 0, size]                            # Axis range
-fig, ax = plt.subplots(figsize=(10, 8))
+
+"============================================================================"
+"10|PLOT AD-AS MODEL"
+### AXIS RANGE
+axis_range = [0, 80, 0, size]
+
+### BUILD PLOT
+fig, ax = plt.subplots(figsize=(10, 8), dpi=dpi)
 ax.set(title="AD-AS MODEL", ylabel=r'$P$', xlabel="Output")
 plt.plot(AD_vector, P_vector, "k-", alpha = 0.7)
 plt.plot(SRAS     , P_vector, "b-", alpha = 0.7)
 plt.axvline(x = LRAS, color = 'r', alpha = 0.7)
 plt.axhline(y = Pstar, xmax = LRAS/80, ls=':', color='k')
-ax.yaxis.set_major_locator(plt.NullLocator())   # Hide ticks
-ax.xaxis.set_major_locator(plt.NullLocator())   # Hide ticks
+### AXIS
+ax.yaxis.set_major_locator(plt.NullLocator())  # Hide ticks
+ax.xaxis.set_major_locator(plt.NullLocator())  # Hide ticks
+### LABELS
 plt.text(75,  2.5, "AD")
 plt.text(45, 11.0, "SRAS", color='b')
 plt.text(30, 11.0, "LRAS", color='r')
-plt.axis(v)
+### SETTINGS
+plt.axis(axis_range)
 plt.show()
 
-#%%
-"9|FUNCTIONS"
-# Define domain of the model
-size = np.round(Pstar, 0)*2
-P_vector = np.linspace(1, size, 500)    # 500 dots between 1 and size
 
-# Agregate Supply and Aggregate Demand
-LRAS = output(N(Pstar))
-SRAS = output(N(P_vector))
-AD_vector = AD(P_vector)
-
-"10|CALCULATE SHOCK EFFECTS"
+#%% *** CELL 3 ***
+"============================================================================"
+"11|CALCULATE SHOCK EFFECTS"
 real_shock    = 1.10
 nominal_shock = 1.10
 
@@ -224,8 +248,8 @@ Nstar2 = N(Pstar2)
 LRAS2  = output2(N(Pstar2))
 SRAS2  = output2(N(P_vector))
 
-gP2 = Pstar2/Pstar - 1                          # Percent change in P
-gY2 = LRAS2/LRAS - 1                            # Percent change in Y 
+gP2 = Pstar2/Pstar - 1  # Percent change in P
+gY2 = LRAS2/LRAS - 1    # Percent change in Y 
 
 # Nominal shock
 M3 = (B * nominal_shock) * m
@@ -247,14 +271,20 @@ Pstar3 = sol_nominal.x
 Nstar3 = N(Pstar3)
 AD_vector3 = AD3(P_vector)
 
-gP3 = Pstar3/Pstar - 1                          # Percent change in P
-gY3 = output(Pstar3)/output(Pstar) - 1          # Percent change in Y 
+gP3 = Pstar3/Pstar - 1                  # Percent change in P
+gY3 = output(Pstar3)/output(Pstar) - 1  # Percent change in Y 
 
-"11|PLOT AD-AS MODEL WITH SHOCKS"
+
+"============================================================================"
+"12|PLOT AD-AS MODEL WITH SHOCKS"
 P3_stop = output(N(Pstar3))
 
-v = [0, 80, 0, size]                            # Axis range
-fig, ax = plt.subplots(nrows=2, figsize=(10, 20))
+### AXIS RANGE
+axis_range = [0, 80, 0, size]
+
+### BUILD PLOT
+fig, ax = plt.subplots(nrows=2, figsize=(10, 20), dpi=dpi)
+### SUBPLOT 1
 ax[0].set(title="AD-AS MODEL: REAL SHOCK", ylabel=r'$P$', xlabel="Output")
 ax[0].plot(AD_vector  , P_vector, "k-", alpha = 0.7)
 ax[0].plot(SRAS       , P_vector, "b-", alpha = 0.7)
@@ -263,14 +293,18 @@ ax[0].axvline(x = LRAS  , ls="-", color='r', alpha = 0.7)
 ax[0].axvline(x = LRAS2 , ls=":", color="r", alpha = 0.7)
 ax[0].axhline(y = Pstar , xmax = LRAS/80 , ls=":", color="k", alpha = 0.7)
 ax[0].axhline(y = Pstar2, xmax = LRAS2/80, ls=":", color="k", alpha = 0.7)
+### SUBPLOT 1: LABELS
 ax[0].text(75.0,  2.5, "AD")
 ax[0].text(44.5, 11.5, "SRAS" , color = "b")
 ax[0].text(48.0, 11.0 , "SRAS'", color = "b")
 ax[0].text(30.0, 11.0, "LRAS" , color = "r")
 ax[0].text(39.0, 11.0, "LRAS'", color = "r")
+### SUBPLOT 1: AXIS
 ax[0].yaxis.set_major_locator(plt.NullLocator())   # Hide ticks
 ax[0].xaxis.set_major_locator(plt.NullLocator())   # Hide ticks
-ax[0].axis(v)
+### SUBPLOT 1: SETTINGS
+ax[0].axis(axis_range)
+### SUIBPLOT 2
 ax[1].set(title="AD-AS MODEL: NOMINAL SHOCK", ylabel=r'$P$', xlabel="Output")
 ax[1].plot(AD_vector  , P_vector, "k-", alpha = 0.7)
 ax[1].plot(AD_vector3 , P_vector, "k:", alpha = 0.7)
@@ -278,11 +312,16 @@ ax[1].plot(SRAS       , P_vector, "b-", alpha = 0.7)
 ax[1].axvline(x = LRAS  , ls="-", color="r", alpha = 0.7)
 ax[1].axhline(y = Pstar , xmax = LRAS/80, ls=":", color="k", alpha = 0.7)
 ax[1].axhline(y = Pstar3, xmax = P3_stop/80, ls=":", color="k", alpha = 0.7)
+### SUBPLOT 2: LABELS
 ax[1].text(75,  1.7, "AD")
 ax[1].text(75,  2.7, "AD'")
 ax[1].text(45, 11.0, "SRAS" , color = "b")
 ax[1].text(30, 11.0, "LRAS" , color = "r")
+### SUBPLOT 2: AXIS
 ax[1].yaxis.set_major_locator(plt.NullLocator())   # Hide ticks
 ax[1].xaxis.set_major_locator(plt.NullLocator())   # Hide ticks
-ax[1].axis(v)
+### SUBPLOT 2: SETTINGS
+ax[1].axis(axis_range)
 plt.show()
+
+
